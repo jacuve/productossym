3771ae8c5be138f182d0a4fb80148d2149d86bbb
Jose A. Cuenca <jacuve@gmail.com>
2026-04-04 09:24:50 +0200

    Connect data

--- src/Controller/DetailController.php
+++ src/Controller/DetailController.php
@@ -4,21 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\ProductoRepository;
 use Symfony\Component\Routing\Attribute\Route;
 
 class DetailController extends AbstractController
 {
-    // El parámetro {id} en la URL se mapea automáticamente al argumento $id
     #[Route('/detalles/{id}', name: 'app_detail')]
-    public function index(int $id): Response
+    public function index(int $id, ProductoRepository $productoRepository): Response
     {
-        // Simulamos un objeto de base de datos
-        $producto = [
-            'id' => $id,
-            'nombre' => 'Laptop Gaming',
-            'precio' => 1200.50,
-            'stock' => true
-        ];
+        $producto = $productoRepository->find($id);
+
+        if (!$producto) {
+            throw $this->createNotFoundException('Producto no encontrado');
+        }
 
         return $this->render('detail.html.twig', [
             'producto' => $producto
